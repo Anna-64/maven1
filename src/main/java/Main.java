2989ec97f97146fad29b54175d075f1b6ccc99bc
Anna-64 <shurygina6410@gmail.com>
2022-01-20 19:03:57 +0400

    Initial

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+  public static void main(String[] args)
+  {BonusService service = new BonusService();
+
+
+    long amount = 1000_60;
+    boolean registered = true;
+    System.out.println(" Количество бонусов: " + amount);
+
+    //   service.calculate(amount, registered);
+  }
+}
+
