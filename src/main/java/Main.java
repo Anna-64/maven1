8bd68894385d11337d3b2b78772345203bd027ac
Anna-64 <shurygina6410@gmail.com>
2022-01-20 23:23:29 +0400

    Initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,8 +5,7 @@ public class Main {
 
     long amount = 1000_60;
     boolean registered = true;
-    System.out.println(" Количество бонусов: " + amount);
-
+    System.out.println(service.calculate(amount, registered));
     //   service.calculate(amount, registered);
   }
 }
